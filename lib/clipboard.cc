4d369733d1907b119db8d2d3d63e60387229d93c
ksx123 <ksx826@gmail.com>
2018-11-01 18:10:51 +0800

    getData support CF_BITMAP

--- lib/clipboard.cc
+++ lib/clipboard.cc
@@ -198,6 +198,83 @@ UINT GetFormatId( const std::wstring &formatName ) {
 	return formatId;
 }
 
+BOOL SaveHBITMAPToChatPT(HBITMAP hBitmap, char ** out, int * size) {
+	HDC hDC;
+	int iBits;
+	WORD wBitCount;
+	DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
+	BITMAP Bitmap0;
+	BITMAPFILEHEADER bmfHdr;
+	BITMAPINFOHEADER bi;
+	LPBITMAPINFOHEADER lpbi;
+	HANDLE hDib, hPal, hOldPal2 = NULL;
+	hDC = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
+	iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES);
+	DeleteDC(hDC);
+	if (iBits <= 1)
+		wBitCount = 1;
+	else if (iBits <= 4)
+		wBitCount = 4;
+	else if (iBits <= 8)
+		wBitCount = 8;
+	else
+		wBitCount = 24;
+	GetObject(hBitmap, sizeof(Bitmap0), (LPSTR)&Bitmap0);
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = Bitmap0.bmWidth;
+	bi.biHeight = -Bitmap0.bmHeight;
+	bi.biPlanes = 1;
+	bi.biBitCount = wBitCount;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrImportant = 0;
+	bi.biClrUsed = 256;
+	dwBmBitsSize = ((Bitmap0.bmWidth * wBitCount + 31) & ~31) / 8
+		* Bitmap0.bmHeight;
+	hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
+	lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
+	*lpbi = bi;
+
+	hPal = GetStockObject(DEFAULT_PALETTE);
+	if (hPal) {
+		hDC = GetDC(NULL);
+		hOldPal2 = SelectPalette(hDC, (HPALETTE)hPal, FALSE);
+		RealizePalette(hDC);
+	}
+
+	GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap0.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
+		+ dwPaletteSize, (BITMAPINFO *)lpbi, DIB_RGB_COLORS);
+
+	if (hOldPal2) {
+		SelectPalette(hDC, (HPALETTE)hOldPal2, TRUE);
+		RealizePalette(hDC);
+		ReleaseDC(NULL, hDC);
+	}
+
+	bmfHdr.bfType = 0x4D42; // "BM"
+	dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
+	bmfHdr.bfSize = dwDIBSize;
+	bmfHdr.bfReserved1 = 0;
+	bmfHdr.bfReserved2 = 0;
+	bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
+
+
+	*size = sizeof(BITMAPFILEHEADER) + dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER);
+
+	char *buffer = (char *)malloc(*size);
+    memset(buffer, 0, *size);
+
+	memcpy(buffer, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER));
+	char * aa = buffer + sizeof(BITMAPFILEHEADER);
+	memcpy(aa, (LPSTR)lpbi, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
+	GlobalUnlock(hDib);
+	GlobalFree(hDib);
+	*out = buffer;
+	return TRUE;
+}
+
 void GetData( const FunctionCallbackInfo<Value> &args ) {
 	Isolate *isolate = args.GetIsolate();
 	Local<v8::Object> ret;
@@ -221,7 +298,16 @@ void GetData( const FunctionCallbackInfo<Value> &args ) {
 
 	UINT formatId = GetFormatId( formatNameUtf16 );
 
-	if ( formatId != 0 ) {
+	if (formatId == 2) {
+      HBITMAP hBitmap = (HBITMAP)GetClipboardData( formatId );
+      if ( hBitmap != NULL ) {
+        char * data;
+        int size;
+        SaveHBITMAPToChatPT(hBitmap, &data, &size);
+        ret = Nan::CopyBuffer( (const char*)data, size ).ToLocalChecked();
+        free(data);
+      }
+  } else if ( formatId != 0 ) {
 		HGLOBAL clipboardDataHandle = GetClipboardData( formatId );
 
 		if ( clipboardDataHandle != NULL ) {
